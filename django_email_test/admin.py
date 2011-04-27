35860824d27ffe41f105c95f19c79eacab866918
Sean Hayes <sean@seanhayes.name>
2011-04-26 20:33:20 -0400

    admin tweaks

--- django_email_test/admin.py
+++ django_email_test/admin.py
@@ -16,6 +16,15 @@ class TestEmailAdmin(admin.ModelAdmin):
 	list_filter = ['sent']
 	readonly_fields = ['added', 'sent', 'error']
 	save_as = True
-	search_fields = ['subject', 'from_email', 'to', 'bcc', 'body']
+	search_fields = ['subject', 'from_email', 'to', 'bcc', 'body', 'error']
+	
+	def formfield_for_dbfield(self, db_field, **kwargs):
+		if db_field.name == 'to':
+			request = kwargs.get('request', None)
+			
+			if request is not None:
+				db_field.default = request.user.email
+		
+		return super(TestEmailAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 admin.site.register(TestEmail, TestEmailAdmin)
